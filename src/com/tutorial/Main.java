ca780966b3969b09a744f8a9b7c21ed0379e963d
hassan.adel <hasanadel555@hotmail.com>
2020-03-28 05:14:36 +0200

    Generic Classes

--- src/com/tutorial/Main.java
+++ src/com/tutorial/Main.java
@@ -1,8 +1,13 @@
 package com.tutorial;
 
+import com.tutorial.generics.GenericsList;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        var myList = new GenericsList<Integer>();
+        myList.add(12);
+        int test = myList.get(0);
     }
 }
